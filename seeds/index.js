0f5bc389be435e9f2972d207bdc6688f7c301bf1
madmax273 <punitay273@gmail.com>
2023-02-04 17:08:25 +0530

    second commit

--- seeds/index.js
+++ seeds/index.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 const cities = require('./cities');
 const { places, descriptors } = require('./seedHelpers');
-const Campground = require('../models/campground');
+const Campground = require('../models/campground');   //DOUBLE .. FOR 
 
-mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp', {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true
@@ -20,7 +20,8 @@ const sample = array => array[Math.floor(Math.random() * array.length)];
 
 
 const seedDB = async () => {
-    await Campground.deleteMany({});
+    await Campground.deleteMany({});   //WILL DELETE ANYTHING IN OUT CAMP GROUNDS DATABASE AND INSERT 50 NEW 
+    
     for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
         const camp = new Campground({
@@ -29,8 +30,12 @@ const seedDB = async () => {
         })
         await camp.save();
     }
+   
 }
 
+//THE ABOVE CHUNK OF CODE WILL ADD 50 CITIES RANDOMLY IN OUR DATABASE campgrounds
+
 seedDB().then(() => {
-    mongoose.connection.close();
+    // mongoose.connection.close();
+    console.log("inserted");
 })
